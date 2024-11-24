8f03012aeaf1bc9eed4f0210ac0d7923dce7e973
pd162 <50524603+pd162@users.noreply.github.com>
2024-11-24 15:45:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
-#  demo
+#  Calculate Macro F-score
+
 import torch
 
 from model import Matcher, Matcher_wdq
-from dataset import MatchDataset, MatchDataset_DLA
+from dataset import MatchDataset, MatchDataset_CDLA
 from torch.utils.data import DataLoader
 import numpy as np
 import cv2
 from PIL import Image
 from collections import OrderedDict
 import torch.nn as nn
+from tqdm import tqdm
+
+from metric import SimpleAccuracy
 
 if __name__ == '__main__':
     device = 'cuda'
     model = Matcher_wdq().to(device)
-    val_dataset = MatchDataset_DLA('test_dla.json', training=False)
-    val_dataloader = DataLoader(val_dataset, batch_size=1, num_workers=1)
+    val_dataset = MatchDataset_CDLA('test_cdla.json', training=False)
+    val_dataloader = DataLoader(val_dataset, batch_size=1, num_workers=8)
 
-    model_path = '/data1/ljh/code/roi_matching/ckpt/checkpoint-best_22_0.04839949918199683.pth'
+    model_path = '/data1/ljh/code/roi_matching/ckpt/checkpoint-best_7_0.7088204638458023.pth'
     state_dict = torch.load(model_path)
 
     temp_state = OrderedDict()
@@ -27,16 +31,29 @@ if __name__ == '__main__':
     model.load_state_dict(temp_state, strict=False)
 
     model.eval()
+    eval_metric = SimpleAccuracy()
 
+    pred_list = []
+    gt_list = []
     with torch.no_grad():
-        for batch_idx, batch_samples in enumerate(val_dataloader):
+
+        for batch_idx, batch_samples in tqdm(enumerate(val_dataloader)):
             test_img, ref_img, test_mask, ref_mask = batch_samples['test_img'], batch_samples['ref_img'], \
                                                      batch_samples['test_mask'], batch_samples['ref_mask']
             test_img, ref_img, test_mask, ref_mask = test_img.to(device), ref_img.to(device), test_mask.to(
                 device), ref_mask.to(device)
             pred = model(ref_img, test_img, ref_mask)
-            # test_mask = (test_mask != 0).any(dim=1).long()  # 凑合用
+            test_mask = (test_mask != 0).any(dim=1).long()  # 凑合用
             pred = pred.cpu().data.numpy()
             pred = np.argmax(pred, axis=1)
 
-            print(0)
+            gt_list.append(test_mask[0].cpu().numpy())
+            pred_list.append(pred[0])
+
+    results = eval_metric.process(pred_list, gt_list)
+    metrics = eval_metric.compute_metrics(results)
+    print(metrics)
+
+
+
+
